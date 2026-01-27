028279fb5d7da9e234ded2a80b88f405c5dae1e3
LevisOgembo <ogembolevis97@gmail.com>
2026-01-27 10:11:56 +0300

    implemented event handlers and component rendering in react

--- src/index.js
+++ src/index.js
@@ -2,44 +2,66 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-const firstBook = {
-  title: "The let them theory",
-  author: "Mel Robbins",
-  img: "https://images-na.ssl-images-amazon.com/images/I/91ZVf3kNrcL._AC_UL900_SR900,600_.jpg"
-}
-
-const secondBook = {
-  title: "Dog Man: Big Jim Believes: A Graphic Novel",
-  author: "Dav Pilkey",
-  img: "https://images-na.ssl-images-amazon.com/images/I/91cUuNGL-GL._AC_UL900_SR900,600_.jpg"
-}
-
+const BookArr = [
+  { 
+    id: 1,
+    title: "Dog Man: Big Jim Believes: A Graphic Novel",
+    author: "Dav Pilkey",
+    img: "https://images-na.ssl-images-amazon.com/images/I/91cUuNGL-GL._AC_UL900_SR900,600_.jpg"
+  },
+  {
+    id: 2,
+    title: "The let them theory",
+    author: "Mel Robbins",
+    img: "https://images-na.ssl-images-amazon.com/images/I/91ZVf3kNrcL._AC_UL900_SR900,600_.jpg"
+  }
+]
 
 const BookList = () => {
   return (
     <section className="bookList">
-      <Book title={firstBook.title} author={firstBook.author} img={firstBook.img}>
-      <p>Lorem ipsum sit amet conceretuer</p>
-      <button onClick={()=>console.log("new button")
-      }>Click Me</button>
-      </Book>
-      <Book title={secondBook.title} author={secondBook.author} img={secondBook.img}/>
-      </section>
+      <EventExamples/>
+    {
+      BookArr.map((book)=>{
+        return (
+          <Book {...book} key={book.id}/>
+        )
+      })
+    }
+    </section>
   );
 };
 
+const EventExamples = () =>{
+  const handleFormInput = () => {
+    console.log('handling form input');
+    
+  }
+
+  const handleButtonClick = () => {
+    alert('button click')
+  }
+
+  return <section>
+    <form>
+      <h2>Typical form</h2>
+      <input type="text" name="example" style={{margin:'1rem 0'}} onChange={handleFormInput}></input>
+    </form>
+    <button onClick={handleButtonClick}>click me</button>
+  </section>
+}
 
 const Book = (props) => {
-  console.log(props);
-  const {img,children} = props
+  //console.log('props',props);
+  const { img, children,title, author } = props
   return (
     <article className="book">
       <img
         src={img}
         alt="let them live"
       />
-      <h2>{props.title}</h2>
-      <p>{props.author}</p>
+      <h2>{title}</h2>
+      <p>{author}</p>
       {children}
     </article>
   );
